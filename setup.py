6ad3e0772b415e2644534fcaead490d77e6fba67
Ronan Klyne <rk-git@rklyne.net>
2012-02-09 15:51:03 +0000

    Added bits of packaging+distribution framework.

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+from distutils.core import setup
+
+setup(
+    name='python-graphite',
+    version='0.2.0',
+    author='Ronan Klyne',
+    author_email='python-graphite@rklyne.net',
+    packages=['graphite'],
+    scripts=[],
+    url='http://code.google.com/p/python-graphite/',
+    license='LICENSE.txt',
+    description='A flexible RDF hacking library built on JPype and Jena',
+    long_description=open('README.txt').read(),
+)
